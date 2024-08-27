f4672813eac3eb5b2ac8eb84a1b4bb62c56589d1
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-08-27 10:44:11 +0530

    made the client and server structure

--- server/api/routes/driver.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createDriverData, getDriverData } = require('../controllers/driverController');
-
-router.post('/', createDriverData);
-router.get('/', getDriverData);
-
-module.exports = router;

4d2e20f9db327b68ebdcb3558e1098a5bb26dc31
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-09-04 09:27:03 +0530

    commit

--- DriveIQ/DriveIQ-backend/utils/ml_integration.py
+++ DriveIQ/DriveIQ-backend/utils/ml_integration.py
@@ -7,9 +7,9 @@ def load_model():
     return model
 
 def predict_driver_behavior(model, features):
-    # Convert features to numpy array for model prediction
+   
     features_array = features[['Speed(m/s)', 'Acceleration(m/s^2)', 'Heading_Change(degrees)', 'Jerk(m/s^3)']].values
     prediction = model.predict(features_array)
-    score = np.mean(features['Driving_Score'])  # Calculate the mean driving score for the trip
-    behavior = features['Driving_Category'].mode()[0]  # Most frequent driving category for the trip
+    score = np.mean(features['Driving_Score'])  
+    behavior = features['Driving_Category'].mode()[0]  
     return {"behavior": behavior, "score": score}
